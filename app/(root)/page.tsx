9e58aa53c36aae4099d9e816f1143e6f00cfb4ae
kumar4532 <krantikumar2001@gmail.com>
2024-12-23 19:49:47 +0530

    add documents

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,50 @@
+import AddDocumentBtn from '@/components/AddDocumentBtn';
+import Header from '@/components/Header'
+import { SignedIn, UserButton } from '@clerk/nextjs'
+import { currentUser } from '@clerk/nextjs/server';
+import Image from 'next/image';
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-const page = () => {
+const page = async() => {
+  const clerkUser = await currentUser();
+  if(!clerkUser) return redirect('/sign-in')
+
+  const documents = [];
+
   return (
-    <div>page</div>
+    <main className='home-container'>
+      <Header className='sticky left-0 top-0'>
+        <div className='flex items-center gap-2 lg:gap-4'>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </Header>
+
+      {
+        documents.length > 0 ? (
+          <div>
+
+          </div>
+        ) : (
+          <div className='document-list-empty'>
+            <Image
+              src='/assets/icons/doc.svg'
+              alt='Document'
+              width={40}
+              height={40}
+              className='mx-auto' 
+            />
+
+            <AddDocumentBtn 
+              userId={clerkUser.id}
+              email={clerkUser.emailAddresses[0].emailAddress}
+            />
+          </div>
+        )
+      }
+    </main>
   )
 }
 
