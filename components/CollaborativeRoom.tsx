940f882da8f655d2f6025787f36c1b364ac89fda
kumar4532 <krantikumar2001@gmail.com>
2024-12-30 23:54:29 +0530

    add edit document name

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -4,17 +4,65 @@ import { ClientSideSuspense, RoomProvider } from '@liveblocks/react'
 import { Editor } from '@/components/editor/Editor'
 import Header from '@/components/Header'
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import ActiveCollaborators from './ActiveCollaborator'
+import { Input } from './ui/input'
+import { currentUser } from '@clerk/nextjs/server'
+import Image from 'next/image'
 
 const CollaborativeRoom = ({ roomId, roomMetadata }: CollaborativeRoomProps) => {
+    const currentUserType = 'editor'
+
+    const [documentTitle, setDocumentTitle] = useState(roomMetadata.title)
+    const [editing, setEditing] = useState(false) 
+    const [loading, setLoading] = useState(false)
+
+    const containerRef = useRef<HTMLDivElement>(null);
+    const inputRef = useRef<HTMLDivElement>(null);
+
+    const updateTitleHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {
+
+    }
+
     return (
         <RoomProvider id={roomId}>
             <ClientSideSuspense fallback={<div>Loading…</div>}>
                 <div className='collaborative-room'>
                     <Header>
-                        <div className='flex w-fit items-center justify-center gap-2'>
-                            <p className='document-title'>This is Shit</p>
+                        <div ref={containerRef} className='flex w-fit items-center justify-center gap-2'>
+                            {
+                                editing && !loading ? (
+                                    <Input 
+                                        type='text'
+                                        value={documentTitle}
+                                        ref={inputRef}
+                                        placeholder='Enter Title'
+                                        onChange={(e) => setDocumentTitle(e.target.value)}
+                                        onKeyDown={updateTitleHandler}
+                                        disabled={!editing}
+                                        className='document-title-input'
+                                    />
+                                ) : (
+                                    <p className='document-title'>{documentTitle}</p>
+                                )
+                            }
+
+                            {currentUserType === 'editor' && !editing && (
+                                <Image 
+                                    src='/assets/icons/edit.svg'
+                                    alt='edit'
+                                    width={24}
+                                    height={24}
+                                    onClick={() => setEditing(true)}
+                                    className='pointer'
+                                />
+                            )}
+
+                            {currentUserType !== 'editor' && !editing && (
+                                <p className='view-only-tag'>View Only</p>
+                            )}
+
+                            {loading && <p className='text-sm text-gray-400'>saving...</p>}
                         </div>
                         <div className='flex w-full flex-1 justify-end gap-2 sm:gap-3'>
                             <ActiveCollaborators />
