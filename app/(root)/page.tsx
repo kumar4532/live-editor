91d7212fbc670fb22a71bba1d32e8e005f6a0163
kumar4532 <krantikumar2001@gmail.com>
2024-12-31 23:24:12 +0530

    add get documents for home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,21 @@
 import AddDocumentBtn from '@/components/AddDocumentBtn';
 import Header from '@/components/Header'
+import { getDocuments } from '@/lib/actions/room.actions';
+import { dateConverter } from '@/lib/utils';
 import { SignedIn, UserButton } from '@clerk/nextjs'
 import { currentUser } from '@clerk/nextjs/server';
 import Image from 'next/image';
+import Link from 'next/link';
 import { redirect } from 'next/navigation';
 import React from 'react'
 
-const page = async() => {
+const page = async () => {
   const clerkUser = await currentUser();
-  if(!clerkUser) return redirect('/sign-in')
+  if (!clerkUser) return redirect('/sign-in')
 
-  const documents = [];
+  const roomDocuments = await getDocuments(clerkUser.emailAddresses[0].emailAddress);
+  console.log(roomDocuments);
+
 
   return (
     <main className='home-container'>
@@ -23,9 +28,37 @@ const page = async() => {
       </Header>
 
       {
-        documents.length > 0 ? (
-          <div>
-
+        roomDocuments.data.length > 0 ? (
+          <div className='document-list-container'>
+            <div className='document-list-title'>
+              <h3 className='text-28-semibold'>All Documents</h3>
+              <AddDocumentBtn
+                userId={clerkUser.id}
+                email={clerkUser.emailAddresses[0].emailAddress}
+              />
+            </div>
+            <ul className='document-ul'>
+              {
+                roomDocuments.data.map(({ id, metadata, createdAt}: any) => (
+                  <li key={id} className='document-list-item'>
+                    <Link href={`/documents/${id}`} className='flex flex-1 items-center gap-4'>
+                      <div className='hidden rounded-md bg-dark-500 p-2 sm:block'>
+                        <Image 
+                          src='/assets/icons/doc.svg'
+                          alt='file'
+                          width={40}
+                          height={40}
+                        />
+                      </div>
+                      <div className='space-y-1'>
+                        <p className='line-clamp-1 text-lg'>{metadata.title}</p>
+                        <p className='text-sm font-light text-blue-100'>Created about {dateConverter(createdAt)}</p>
+                      </div>
+                    </Link>
+                  </li>
+                ))
+              }
+            </ul>
           </div>
         ) : (
           <div className='document-list-empty'>
@@ -34,10 +67,10 @@ const page = async() => {
               alt='Document'
               width={40}
               height={40}
-              className='mx-auto' 
+              className='mx-auto'
             />
 
-            <AddDocumentBtn 
+            <AddDocumentBtn
               userId={clerkUser.id}
               email={clerkUser.emailAddresses[0].emailAddress}
             />
