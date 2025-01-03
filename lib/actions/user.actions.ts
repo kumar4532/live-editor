a1518f5c28da85662cff522db23e39c690375dbd
kumar4532 <krantikumar2001@gmail.com>
2025-01-03 20:10:20 +0530

    fix and user mentions

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -2,6 +2,7 @@
 
 import { clerkClient } from "@clerk/nextjs/server";
 import { parseStringify } from "../utils";
+import { liveblocks } from "../liveblocks";
 
 export const getClerkUsers = async ({ userIds }: { userIds: string[] }) => {
     try {
@@ -23,5 +24,25 @@ export const getClerkUsers = async ({ userIds }: { userIds: string[] }) => {
     } catch (error) {
         console.error(`Error fetching users: ${error}`);
         return [];
+    }
+}
+
+export const getDocumentUsers = async ({ roomId, currentUser, text }: { roomId: string, currentUser: string, text: string }) => {
+    try {
+        const room = await liveblocks.getRoom(roomId)
+
+        const users = Object.keys(room.usersAccesses).filter((email) => email !== currentUser)
+
+        if (text.length) {
+            const lowerCaseText = text.toLowerCase();
+
+            const filteredUsers = users.filter((email: string) => email.toLowerCase().includes(lowerCaseText))
+
+            return parseStringify(filteredUsers)
+        }
+
+        return parseStringify(users)
+    } catch (error) {
+        console.log("Error while fetching document users", error)
     }
 }
