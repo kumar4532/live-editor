8a9e20c6c3e17a3b099dfb50e98f995c79de6fbf
kumar4532 <krantikumar2001@gmail.com>
2025-01-05 00:18:20 +0530

    add notification

--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -85,7 +85,21 @@ export const updateDocumentAccess = async ({ roomId, email, userType, updatedBy 
     const room = await liveblocks.updateRoom(roomId, { usersAccesses })
 
     if (room) {
+      const notificationId = nanoid();
 
+      await liveblocks.triggerInboxNotification({
+        userId: email,
+        kind: '$documentAccess',
+        subjectId: notificationId,
+        activityData: {
+          userType,
+          title: `You have been granted ${userType} access to the document`,
+          updatedBy: updatedBy.name,
+          avatar: updatedBy.avatar,
+          email: updatedBy.email
+        },
+        roomId
+      })
     }
 
     revalidatePath(`/documents/${roomId}`)
